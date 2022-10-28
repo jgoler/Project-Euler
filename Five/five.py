3840057a8594f246d31663e304d248fe133c99e5
jgoler <jack.goler@gmail.com>
2022-10-28 15:04:27 -0700

    Finished problem 5

---
+++ Five/five.py
@@ -0,0 +1,8 @@
+# this is just a math problem, not much coding involved
+factors = [2**4, 3**2, 5, 7, 11, 13, 17, 19]
+product = 1
+for i in range(len(factors)):
+	product *= factors[i]
+print(product)
+
+#solution is 232792560
